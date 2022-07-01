e44bb176e6838f4c1f2fe8881a7dc08f5972b926
Sabbadini10 <matiassabbadini24@gmail.com>
2022-07-01 15:07:08 -0300

    estructura creada de html y css

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath))
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    });
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve('./view/index.html'));
+    });
+
+    app.get('/music.html', (req, res) => {
+        res.sendFile(path.resolve('./view/music.html'));
+    });
+
+    app.get('/about.html', (req, res) => {
+        res.sendFile(path.resolve('./view/about.html'));
+    });
+
+    app.get('/contact.html', (req, res) => {
+        res.sendFile(path.resolve('./view/contact.html'));
+    });
